1a9d4498df3d8c0162e778820c97b68791ee7133
Rob Dale <rob@robdale.me.uk>
2020-09-24 19:31:21 +0100

    Expanded main

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"strings"
+	"errors"
+	"os"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	if err := echo(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "%+vn", err)
+	}
+	os.Exit(1)
+}
+
+func echo(args []string) error {
+	if len(args) < 2 {
+		return errors.New("No message to echo")
+	}
+	_, err := fmt.Println(strings.Join(args[i:], " "))
+	return err
 }
